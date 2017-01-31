cce3ed5e3b1632cfb9d1afbdc509ab0849f1eb6a
Chris <mail@christopherparsons.me>
2017-01-31 10:43:44 +0000

    Base Javascript
    Put in the basics for a countdown clock to March 3rd.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var endtime = '03/03/2017';
+
+function timeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60  % 60));
+	var hours = Math.floor( t/(1000*60*60) % 24);
+	var days = Math.floor ( t/(1000*60*60*24) );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+	console.log(days);
+}
