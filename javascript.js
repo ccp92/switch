e6e712d688ee4b0ad3e59cdf94684bc992002ccd
Chris <mail@christopherparsons.me>
2017-02-01 21:33:46 +0000

    Countdown and Table
    Coundown clock has been added.
    In addition table can now be sorted by any of the fields available.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 var launch = '03/03/2017';
 
-function timeRemaining(endtime) {
-	var t = Date.parse(endtime) - Date.parse(new Date());
-	var seconds = Math.floor( (t/1000) % 60 );
-	var minutes = Math.floor( (t/1000/60  % 60));
-	var hours = Math.floor( t/(1000*60*60) % 24);
-	var days = Math.floor ( t/(1000*60*60*24) );
-    $('#days').text('Days: ' + days);
-    console.log('Days: ' +days);
-}
+$(function() {
+    timeRemaining(launch);
+    $('#game-table').DataTable();
+});
 
-timeRemaining(launch);
+function timeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60  % 60));
+    var hours = Math.floor( t/(1000*60*60) % 24);
+    var days = Math.floor ( t/(1000*60*60*24) );
+    console.log($('#count').text());
+    $('#count').text('Days: ' + days + ' Hours: ' + hours);
+    console.log('Days: ' + days + ' Hours: ' + hours);
+}
